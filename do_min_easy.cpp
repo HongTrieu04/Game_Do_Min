07014b61a873b5ee8e29d2343b4dfea48967d6ec
HongTrieu04 <126672935+HongTrieu04@users.noreply.github.com>
2023-03-09 19:41:26 +0700

    Add files via upload

---
+++ do_min_easy.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Hàm sinh bản đồ mìn
+vector<vector<int>> generate_map(int m, int n, int k) {
+    // Tạo một vector 2 chiều để lưu bản đồ mìn
+    vector<vector<int>> mine_map(m, vector<int>(n, 0));
+    int count = 0;
+    // Sinh ngẫu nhiên K quả mìn
+    while (count < k) {
+        int x = rand() % m;
+        int y = rand() % n;
+        if (mine_map[x][y] == 0) {
+            mine_map[x][y] = 9; // Đánh dấu ô chứa mìn bằng số 9
+            count++;
+        }
+    }
+    // Tính số mìn xung quanh mỗi ô trống
+ /*   for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mine_map[i][j] == 9) continue;
+            int count = 0;
+            if (i > 0 && j > 0 && mine_map[i - 1][j - 1] == 9) count++;
+            if (i > 0 && mine_map[i - 1][j] == 9) count++;
+            if (i > 0 && j < n - 1 && mine_map[i - 1][j + 1] == 9) count++;
+            if (j > 0 && mine_map[i][j - 1] == 9) count++;
+            if (j < n - 1 && mine_map[i][j + 1] == 9) count++;
+            if (i < m - 1 && j > 0 && mine_map[i + 1][j - 1] == 9) count++;
+            if (i < m - 1 && mine_map[i + 1][j] == 9) count++;
+            if (i < m - 1 && j < n - 1 && mine_map[i + 1][j + 1] == 9) count++;
+            mine_map[i][j] = count;
+        }
+    }*/
+    return mine_map;
+}
+
+int main() {
+    // Thiết lập seed cho hàm sinh số ngẫu nhiên
+    srand(time(NULL));
+    // Nhập kích thước bản đồ và số lượng mìn từ bàn phím
+    int m, n, k;
+    cout << "Nhap kich thuoc ban do (m, n) va so luong min (k): ";
+    cin >> m >> n >> k;
+    // Sinh bản đồ mìn
+    vector<vector<int>> mine_map = generate_map(m, n, k);
+    // Lặp đến khi người chơi chọn ô có mìn hoặc đã mở tất cả các ô trống
+    int open_count = 0;
+    while (open_count < m * n - k) {
+        // Hiển thị bản đồ hiện tại
+        cout << "Ban do hien tai:\n";
+        if (open_count == 0) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    cout << " . ";
+                    /*if (mine_map[i][j] == 9) {
+                        cout << " * ";
+                    }
+                    else if (mine_map[i][j] == -1) {
+                        cout << " X ";
+                    }
+                    else if (mine_map[i][j] == 0) {
+                        cout << " . ";
+                    }
+                    else {
+                        cout << " " << mine_map[i][j] << " ";
+                    }*/
+                }
+                cout << endl;
+            }
+        }
+        // Nhập tọa độ ô cần mở từ bàn phím
+        int x, y;
+        do {
+            cout << "Nhap toa do o (x, y): ";
+            cin >> x >> y;
+        } while (x >= m || y >= n || mine_map[x][y] == -2);
+        
+        // Kiểm tra ô có mìn hay không
+        if (mine_map[x][y] == 9) {
+            mine_map[x][y] = -1;
+            cout << "YOU'RE DEAD!" << endl;
+            // In lại bản đồ với tất cả các quả mìn ra màn hình
+            cout << "Ban do hoan tat:\n";
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mine_map[i][j] == 0) {
+                        cout << " . ";
+                    }
+                    else if (mine_map[i][j] == -1) {
+                        cout << " X ";
+                    }
+                    else if (mine_map[i][j] == -2) {
+                        cout << " O ";
+                    }
+                    else if (mine_map[i][j] == 9) {
+                        cout << " * ";
+                    }
+                }
+                cout << endl;
+            }
+            break; // Kết thúc trò chơi
+        }
+        else {
+            mine_map[x][y] = -2;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mine_map[i][j] == -2) cout << " O ";
+                    else cout << " . ";
+                }
+                cout << endl;
+            }
+            open_count++;
+        }
+    }
+    if (open_count == m * n - k) {
+        cout << "YOU WIN" << endl << "Ban do hoan tat:" << endl;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mine_map[i][j] == 9) cout << " * ";
+                else cout << " O ";
+            }
+            cout << endl;
+        }
+    }
+}
---
+++ do_min_hard.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// khai báo kích thước bàn cờ
+const int ROWS = 10;
+const int COLS = 10;
+const int NUM_MINES = 10;
+
+// khai báo kiểu dữ liệu ô cờ
+struct Cell {
+    bool isMine; // có mìn hay không
+    bool isRevealed; // đã được mở ra hay chưa
+    int numAdjacentMines; // số lượng ô có mìn xung quanh
+};
+
+// tạo bàn cờ mới với các ô chưa được mở ra
+vector<vector<Cell>> createBoard() {
+    vector<vector<Cell>> board(ROWS, vector<Cell>(COLS));
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+            board[row][col].isMine = false;
+            board[row][col].isRevealed = false;
+            board[row][col].numAdjacentMines = 0;
+        }
+    }
+    return board;
+}
+
+// tạo mìn ngẫu nhiên trên bàn cờ
+void placeMines(vector<vector<Cell>>& board, int numMines) {
+    int numPlacedMines = 0;
+    while (numPlacedMines < numMines) {
+        int row = rand() % ROWS;
+        int col = rand() % COLS;
+        if (!board[row][col].isMine) {
+            board[row][col].isMine = true;
+            numPlacedMines++;
+        }
+    }
+}
+
+// tính số lượng ô có mìn xung quanh của từng ô trên bàn cờ
+void calculateAdjacentMines(vector<vector<Cell>>& board) {
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+            if (board[row][col].isMine) {
+                continue; // nếu ô hiện tại là mìn thì bỏ qua
+            }
+            int count = 0;
+            for (int r = row - 1; r <= row + 1; r++) {
+                for (int c = col - 1; c <= col + 1; c++) {
+                    if (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c].isMine) {
+                        count++;
+                    }
+                }
+            }
+            board[row][col].numAdjacentMines = count;
+        }
+    }
+}
+
+// hiển thị bàn cờ
+void displayBoard(vector<vector<Cell>>& board) {
+    cout << "    ";
+    for (int j = 0; j < COLS; ++j) {
+        cout << j << " ";
+    }
+    cout << endl;
+    cout << "   ";
+    for (int j = 0; j < COLS; ++j) {
+        cout << "__";
+    }
+    cout << endl;
+    for (int i = 0; i < ROWS; ++i) {
+        cout << i << " | ";
+        for (int j = 0; j < COLS; ++j) {
+            if (!board[i][j].isRevealed) {
+                cout << ". ";
+            }
+            else if (board[i][j].isMine) {
+                cout << "* ";
+            }
+            else {
+                cout << board[i][j].numAdjacentMines << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// mở một ô cờ
+bool revealCell(vector<vector<Cell>>& board, int row, int col) {
+    if (board[row][col].isMine) {
+        board[row][col].isRevealed = true;
+        return false; // ô hiện tại là mìn
+    }
+    if (board[row][col].isRevealed) {
+        return true; // ô đã được mở trước đó
+    }
+    board[row][col].isRevealed = true;
+    if (board[row][col].numAdjacentMines == 0) {
+        // nếu ô không có mìn xung quanh, tiến hành mở các ô xung quanh
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                if (r >= 0 && r < ROWS && c >= 0 && c < COLS) {
+                    revealCell(board, r, c);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    srand(time(NULL)); // khởi tạo seed cho hàm rand()
+    vector<vector<Cell>> board = createBoard();
+    placeMines(board, NUM_MINES);
+    calculateAdjacentMines(board);
+    bool gameOver = false;
+    while (!gameOver) {
+        displayBoard(board);
+        int row, col;
+        cout << "Enter row and column (separated by space): ";
+        cin >> row >> col;
+        gameOver = !revealCell(board, row, col);
+    }
+    cout << "Game over!" << endl;
+    return 0;
+}
+
